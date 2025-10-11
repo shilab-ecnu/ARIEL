95baa06536cdc865d9d2daa6c42d6160732a0371
Yin Jun <jj.yin@qq.com>
2025-10-11 11:16:26 +0800

    update readme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,12 +1,5 @@
 # Configuration file for the Sphinx documentation builder.
-import os
-import sys
 
-# -- Path setup --------------------------------------------------------------
-# 如果你的 ariel_srt 包在 src 目录下，可以添加路径（可选）
-# sys.path.insert(0, os.path.abspath('../../src'))
-
-# -- Project information -----------------------------------------------------
 project = 'ARIEL'
 copyright = '2025, Yang Wanshuo, Yin Jun'
 author = 'Yang Wanshuo, Yin Jun'
@@ -14,34 +7,24 @@ author = 'Yang Wanshuo, Yin Jun'
 # The full version, including alpha/beta/rc tags
 release = '0.1.0'
 
-
-# -- General configuration ---------------------------------------------------
-# 只保留 myst-nb，它已包含对 .md 和 .ipynb 的支持
 extensions = [
-    'myst_nb',               # 核心：支持 Markdown 和 Jupyter Notebook
-    'sphinx_rtd_theme',      # 使用 Read the Docs 主题
+    'myst_nb',               
+    'sphinx_rtd_theme',      
 ]
 
-# 设置主页面
 master_doc = 'index'
 
-# 支持的文件后缀
 source_suffix = {
     '.rst': 'restructuredtext',
     '.md': 'markdown',
     '.ipynb': 'myst-nb',
 }
 
-# 语言
 language = 'en'
 
-# 不要尝试执行 notebook（关键！避免内核错误和加速构建）
+# Do not run notebook
 nb_execution_mode = "off"
 
-# 可选：如果你希望缓存执行结果（开发时）
-# nb_execution_cache_path = './_build/.jupyter_cache'
-
-# 忽略某些文件
 exclude_patterns = [
     '_build',
     'Thumbs.db',
@@ -53,10 +36,10 @@ exclude_patterns = [
 # -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
 
-# HTML 输出选项
+templates_path = ['_templates']
 html_static_path = ['_static']
+exclude_patterns = []
 
-# 在侧边栏显示当前页的章节
 html_theme_options = {
     'navigation_depth': 4,
     'collapse_navigation': False,
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,16 +1,23 @@
 Welcome to ARIEL’s documentation!
 =======================================
 
-Welcome to the documentation for the ARIEL project.
+We develop a fast, omics-driven sample integration tool called ARIEL, which enables effortless landmark detection, spatial alignment and information transfer for multi-sample spatial transcriptomic data. ARIEL can be applied in diverse situation, including cross-sample, cross-platform, cross-resolution, cross-omics, and cross-disease scenarios. The main program of ARIEL can be summarized in the figure:
+
+.. image:: _static/ARIEL_hippo12.png
+   :alt:  The main program of ARIEL
+
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
+   readme.ipynb
    brain.ipynb
    DLPFC(Alignment).ipynb
    hepatic_lobule.ipynb
    hippocampus.ipynb
+
+
 
 
 
